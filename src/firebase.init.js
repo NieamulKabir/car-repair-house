dbff406510285c6a814fe806b1c347a6c42cc6e3
NieamulKabir <kabir000079@gmail.com>
2022-09-20 21:14:01 +0600

    inveroment setup

--- src/firebase.init.js
+++ src/firebase.init.js
@@ -1,17 +1,17 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import {getAuth} from 'firebase/auth';
+import { getAuth } from 'firebase/auth';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCS8dTJvHk3g3qEEk1A1RGv3j6ml3akc68",
-  authDomain: "car-repair-house.firebaseapp.com",
-  projectId: "car-repair-house",
-  storageBucket: "car-repair-house.appspot.com",
-  messagingSenderId: "927830024240",
-  appId: "1:927830024240:web:a3fbb16218b8a0e5518df5"
+  apiKey: process.env.REACT_APP_apiKey,
+  authDomain: process.env.REACT_APP_authDomain,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appId,
 };
 
 // Initialize Firebase
